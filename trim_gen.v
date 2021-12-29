37b9795157db008c706184ed9e3905ac2c3c1fff
Daniel Adjei <marshal.adjei21@gmail.com>
2021-12-28 20:24:22 -0600

    Added state transitions and little bit extra

--- trim_gen.v
+++ trim_gen.v
@@ -14,11 +14,15 @@ module trim_gen (CLK50,START,RST,ENCLK,DOUT,TRIMCODE);
     localparam MAX_CLK_COUNT = 25'd25000000;
     localparam MAX_T_COUNT = 4'd12;
 
+    //Internal signals
+    wire [11:0] trim_code
     //Internal storage elements
     reg [11:0] trimcode_hold;
     reg [1:0] state;
     reg div_clk;
     reg [24:0] clk_count;
+    reg [3:0] t;
+    reg load, enable;
 
     //Clock divider
     always @(posedge CLK50 or posedge RST) begin
@@ -34,6 +38,77 @@ module trim_gen (CLK50,START,RST,ENCLK,DOUT,TRIMCODE);
         end
     end
 
+    always @(posedge div_clk or posedge RST) begin
+        
+        //On reset, return to idle state
+        if (RST) begin
+            state <= STATE_IDLE;
+        end
+        //Define the state transitions
+        else begin
+            case (state)
+                STATE_IDLE: begin
+                    trimcode_hold <= 11'd0;
+                    TRIMCODE <= 11'd0;
+                    div_clock <= 1'b0;
+                    clk_count <= 25'd0;
+                    DOUT <= 0;
+
+                    if (START == 1'b1) begin
+                        state <= STATE_INITIAL;
+                    end
+                end 
+
+                STATE_INITIAL: begin
+                    trimcode_hold <= trimcode_hold + 1;
+                    TRIMCODE <= 0;
+                    DOUT <= 1'b0;
+                    state <= STATE_LOAD;
+                end
+
+                STATE_LOAD: begin
+                    state <= STATE_SHIFT;
+                end
+
+                STATE_SHIFT: begin
+                    if (t == MAX_T_COUNT) begin
+                        state <= STATE_INITIAL;
+                    end
+                end
+            endcase
+        end
+    end
+
+    always @(posedge div_clk or posedge RST) begin
+        if (RST) begin
+            t <= 4'd0;
+        end
+        else begin
+            if (state == STATE_SHIFT) begin
+                t <= t + 1;
+            end
+            else begin
+                t <= 4'd0;
+            end
+        end
+    end
+
+
+    always @(*) begin
+        if (state == STATE_LOAD) begin
+            load = 1'b1;
+        end
+        else begin
+            load = 1'b0;
+        end
+
+        if (state == STATE_SHIFT) begin
+            enable = 1'b1;
+        end
+        else begin
+            enable = 1'b0;
+        end
+    end
 
 
 
